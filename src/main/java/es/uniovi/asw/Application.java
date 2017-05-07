cff975cee453d5ea36f38d8b88256a718fe12c12
Seila Khayat Prada <seilakp1996@hotmail.es>
2017-05-08 01:23:03 +0200

    Last try de verdad de la buena :D

--- src/main/java/es/uniovi/asw/Application.java
+++ src/main/java/es/uniovi/asw/Application.java
@@ -4,10 +4,14 @@ package es.uniovi.asw;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.boot.Banner;
 
+@Configuration
 @EnableAutoConfiguration
 @SpringBootApplication
+@ComponentScan
 public class Application {
 
    public static void main(String[] args) {
