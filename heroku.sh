d9f76e2c8e911debc23e010bc2644638eeba0c2d
Seila Khayat Prada <seilakp1996@hotmail.es>
2017-05-08 00:22:47 +0200

    Pq a otros grupos les va y a nosotros no :(

--- heroku.sh
+++ heroku.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
+if [ ! -d kafka ]; then
+    wget http://apache.rediris.es/kafka/0.10.2.0/kafka_2.11-0.10.2.0.tgz -O kafka.tgz
+    mkdir -p kafka && tar xzf kafka.tgz -C kafka --strip-components 1
+    rm kafka.tgz
+fi
+nohup bash -c "cd kafka && bin/zookeeper-server-start.sh config/zookeeper.properties &"
+sleep 10
+nohup bash -c "cd kafka && bin/kafka-server-start.sh config/server.properties &"
 java $JAVA_OPTS -Dserver.port=$PORT -jar target/*.jar
