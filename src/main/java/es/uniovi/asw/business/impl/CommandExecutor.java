8ed9118a05c358eb220c6536d08b4cfe23817494
Moises Muñiz <uo237351@uniovi.es>
2017-05-07 19:10:56 +0200

    Probando cosas nuevas

--- src/main/java/es/uniovi/asw/business/impl/CommandExecutor.java
+++ src/main/java/es/uniovi/asw/business/impl/CommandExecutor.java
@@ -1,7 +1,12 @@
 package es.uniovi.asw.business.impl;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
 import javax.persistence.PersistenceException;
 
 import es.uniovi.asw.infrastructure.MyLogger;
@@ -37,4 +42,5 @@ public class CommandExecutor {
 				mapper.close();
 		}
 	}
+
 }
--- src/main/java/es/uniovi/asw/persistence/util/Jpa.java
+++ src/main/java/es/uniovi/asw/persistence/util/Jpa.java
@@ -1,6 +1,9 @@
 package es.uniovi.asw.persistence.util;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -34,7 +37,7 @@ public class Jpa {
 	private static EntityManagerFactory getEmf() {
 		if (emf == null) {
 			String persistenceUnitName = loadPersistentUnitName();
-			emf = Persistence.createEntityManagerFactory(persistenceUnitName);
+			emf = Persistence.createEntityManagerFactory(persistenceUnitName, getConfForPostgres());
 		}
 		return emf;
 	}
@@ -45,12 +48,11 @@ public class Jpa {
 			DocumentBuilder db = dbf.newDocumentBuilder();
 			Document doc = db.parse(Jpa.class.getResourceAsStream("/META-INF/persistence.xml"));
 			MyLogger.debug("====> Estoy sacando unidad de pesistencia");
-			
 
 			doc.getDocumentElement().normalize();
 			NodeList nl = doc.getElementsByTagName("persistence-unit");
 			MyLogger.debug("Nombre unidad: " + ((Element) nl.item(0)).getAttribute("name"));
-			
+
 			return ((Element) nl.item(0)).getAttribute("name");
 
 		} catch (ParserConfigurationException e1) {
@@ -60,6 +62,31 @@ public class Jpa {
 		} catch (IOException e1) {
 			throw new RuntimeException(e1);
 		}
+	}
+
+	private static Map<String, String> getConfForPostgres() {
+		String databaseUrl = System.getenv("DATABASE_URL");
+
+		StringTokenizer st = new StringTokenizer(databaseUrl, ":@/");
+
+		String userName = st.nextToken();
+		String password = st.nextToken();
+		String host = st.nextToken();
+		String port = st.nextToken();
+		String databaseName = st.nextToken();
+
+		String jdbcUrl = String.format(
+				"jdbc:postgresql://%s:%s/%s?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory", host, port,
+				databaseName);
+
+		Map<String, String> properties = new HashMap<String, String>();
+
+		properties.put("javax.persistence.jdbc.url", jdbcUrl);
+		properties.put("javax.persistence.jdbc.user", userName);
+		properties.put("javax.persistence.jdbc.password", password);
+		properties.put("javax.persistence.jdbc.driver", "org.postgresql.Driver");
+
+		return properties;
 	}
 
 }
