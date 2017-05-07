a04fcf616acab16c84a91a18e62be99a37e7ef7f
Moises Muñiz <uo237351@uniovi.es>
2017-05-07 18:23:39 +0200

    :)

--- src/main/java/es/uniovi/asw/business/impl/CommandExecutor.java
+++ src/main/java/es/uniovi/asw/business/impl/CommandExecutor.java
@@ -4,13 +4,16 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
 import javax.persistence.PersistenceException;
 
+import es.uniovi.asw.infrastructure.MyLogger;
 import es.uniovi.asw.model.exception.BusinessException;
 import es.uniovi.asw.persistence.util.Jpa;
 
 public class CommandExecutor {
 
 	public Object execute(Command cmd) throws BusinessException{
+		MyLogger.debug("Creando Entity manager");
 		EntityManager mapper = Jpa.createEntityManager();
+		MyLogger.debug("Creado el Entity manager");
 		EntityTransaction trx = mapper.getTransaction();
 		trx.begin();
 
