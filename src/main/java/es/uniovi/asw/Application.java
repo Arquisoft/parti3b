ce7402e6323f7645d97b06c3ab98b7b9b1776e8f
Seila Khayat Prada <seilakp1996@hotmail.es>
2017-05-08 03:00:13 +0200

    A la mierda Heroku chavales :P

--- src/main/java/es/uniovi/asw/Application.java
+++ src/main/java/es/uniovi/asw/Application.java
@@ -2,22 +2,14 @@ package es.uniovi.asw;
 
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.boot.Banner;
 
-@Configuration
-@EnableAutoConfiguration
+
 @SpringBootApplication
-@ComponentScan
 public class Application {
 
    public static void main(String[] args) {
-    	//SpringApplication.run(Application.class, args);
-	    SpringApplication app = new SpringApplication(Application.class);
-		app.setBannerMode(Banner.Mode.OFF);
-		app.run(args);
+    	SpringApplication.run(Application.class, args);
+
    }
 }
